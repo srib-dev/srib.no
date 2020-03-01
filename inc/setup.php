a0838cbc6e06a1ca07d5913e3b6bc5cf60e9a041
Tim <tim@adipid.no>
2020-03-01 12:50:40 +0100

    Re-added summary
    Didnt notice it was missing

--- inc/setup.php
+++ inc/setup.php
@@ -121,20 +121,19 @@ if ( ! function_exists( 'understrap_custom_excerpt_more' ) ) {
 
 add_filter( 'wp_trim_excerpt', 'understrap_all_excerpts_get_more_link' );
 
-//if ( ! function_exists( 'understrap_all_excerpts_get_more_link' ) ) {
+if ( ! function_exists( 'understrap_all_excerpts_get_more_link' ) ) {
 	/**
 	 * Adds a custom read more link to all excerpts, manually or automatically generated
 	 *
 	 * @param string $post_excerpt Posts's excerpt.
 	 *
 	 * @return string
-	 */ /*
+	 */ 
 	function understrap_all_excerpts_get_more_link( $post_excerpt ) {
 		if ( ! is_admin() ) {
-			$post_excerpt = $post_excerpt . ' [...]<p><a class="btn btn-secondary understrap-read-more-link" href="' . esc_url( get_permalink( get_the_ID() ) ) . '">' . __( 'Read More...',
-			'understrap' ) . '</a></p>';
+			$post_excerpt = $post_excerpt;
 		}
 		return $post_excerpt;
 	}
 }
-*/
+
